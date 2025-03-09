5d5f21415cd27cd0a89624fc33f3376bd339b15f
Daesiker <eotlr680@naver.com>
2025-03-09 23:10:54 +0900

    BookShop 프로젝트 초기화

---
+++ BookShop/routes/orders.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+router.use(express.json());
+
+router.post('/', (req, res) => {
+
+});
+
+router.get('/', (req, res) => {
+
+});
+
+router.delete('//:id', (req, res) => {
+
+});
+
+
+module.exports = router
---
+++ BookShop/routes/users.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+router.use(express.json());
+
+router.post('/join', (req, res) => {
+
+});
+
+router.post('/login');
+
+router.post('/reset');
+
+router.put('/reset');
+
+module.exports = router
