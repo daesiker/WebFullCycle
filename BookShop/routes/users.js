0441b0e087384a3075d2039792839c43fa61a779
Daesiker <eotlr680@naver.com>
2025-03-10 23:02:18 +0900

    UserController 추가

--- BookShop/routes/users.js
+++ BookShop/routes/users.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const conn = require('../mariadb');
+const {join, login, passwordResetRequest, passwordReset}  = require('../controller/UserController');
 
 router.use(express.json());
 
-router.post('/join', (req, res) => {
+router.post('/join', join);
+router.post('/login', login);
+router.post('/reset', passwordResetRequest);
+router.put('/reset', passwordReset);
 
-});
-
-router.post('/login');
-
-router.post('/reset');
-
-router.put('/reset');
-
-module.exports = router
+module.exports = router//
